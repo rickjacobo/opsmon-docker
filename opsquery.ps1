41c5e7f5b649824b3f3401bf29641447bbbd13f4
rickjacobo <71619054+rickjacobo@users.noreply.github.com>
2022-04-06 05:43:10 -0700

    Update opsquery.ps1

--- opsquery.ps1
+++ opsquery.ps1
@@ -22,6 +22,4 @@ $Table = (Get-Content $Config | ConvertFrom-Json).table
 $Query = "SELECT * FROM [TABLE]"
 $Query = $Query.Replace("[TABLE]","$Table")
 
-#$Date = (get-date).ToUniversalTime()
-#Write-Host "Current UTC Time: $Date" -ForeGroundColor Yellow
-Invoke-SQLQuery -Config $Config -Query $Query | Select hostname,type,port,status,alert,pagerduty_dedup,lastupdate_utc | Format-Table
+Invoke-SQLQuery -Config $Config -Query $Query | Select id,hostname,type,port,status,alert,pagerduty_dedup,lastupdate_utc | Format-Table
